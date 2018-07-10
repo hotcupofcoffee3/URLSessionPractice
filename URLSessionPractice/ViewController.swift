d9ed3b0a26e5a7d65efc036db67b04e8e98a4007
Adam Moore <adammoore@Adams-MacBook-Pro.local>
2018-07-10 15:15:52 -0600

    Initial commit

---
+++ URLSessionPractice/ViewController.swift
@@ -0,0 +1,270 @@
+//
+//  ViewController.swift
+//  URLSessionPractice
+//
+//  Created by Adam Moore on 6/27/18.
+//  Copyright © 2018 Adam Moore. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    @IBOutlet weak var zipTextField: UITextField!
+    
+    @IBAction func getTempButton(_ sender: UIButton) {
+        
+        if let zip = Int(zipTextField.text!) {
+            
+            getTempAndUVIndexFromZip(zip: zip)
+            
+        }
+        
+        zipTextField.text = ""
+        
+    }
+    
+    var city = String()
+    
+    var temperature = Double()
+    
+    var uvIndex = Double()
+    
+    let apiKeys = API()
+    
+    
+    
+    
+    func getTempAndUVIndexFromZip(zip: Int) {
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(String(zip)) { (cLLocations, error) in
+            
+            if error != nil {
+                
+                print(error!)
+                
+            } else {
+                
+                if let locations = cLLocations {
+                    
+                    for cLLocation in locations {
+                        
+                        if let location = cLLocation.location {
+                            
+//                            print("Lat: \(location.coordinate.latitude)")
+//
+//                            print("Lon: \(location.coordinate.longitude)")
+                            self.getUVIndexFromLatAndLon(zip: zip, lat: location.coordinate.latitude, lon: location.coordinate.longitude)
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    func getUVIndexFromLatAndLon(zip: Int, lat: Double, lon: Double) {
+        
+//        print(lat)
+//        print(lon)
+        
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/uvi?appid=\(apiKeys.openWeatherMapAPIKey)&lat=\(lat)&lon=\(lon)")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+//                        print(jsonResult)
+                        
+//                        print(jsonResult["value"])
+                        
+                        if let uv = jsonResult["value"] as? Double {
+
+                            self.uvIndex = uv
+                            
+                        } else {
+                            
+                            self.uvIndex = 0.0
+                            
+                        }
+                        
+//                        print(uvIndexReturned)
+                        
+//                        print(jsonResult["main"])
+                        
+                        DispatchQueue.main.async {
+                            
+                            self.getCurrentTemperature(zipCode: zip)
+                            
+                        }
+                        
+                    } catch {
+                        
+                        print("JSON Processing Error.")
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+    func getCurrentTemperature(zipCode: Int) {
+        
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?zip=\(String(zipCode)),us&appid=\(apiKeys.openWeatherMapAPIKey)")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error != nil {
+                print(error!)
+            } else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+//                        print(jsonResult)
+
+//                        print(jsonResult["name"])
+                        
+                        if let city = jsonResult["name"] as? String {
+                            
+                            self.city = city
+                            
+                        }
+                        
+//                        print(jsonResult["main"])
+                        
+                        if let main = jsonResult["main"] as? [String: Any] {
+                            
+                            if let temp = main["temp"] as? Double {
+                                
+                                self.temperature = (Double(temp) - 273.15)
+                                
+                            }
+                            
+                        }
+
+//                        print(self.temperature)
+
+                        DispatchQueue.main.async {
+
+                            self.tempLabel.text = "\(self.city):\n\(String(self.temperature))°C\nUV: \(self.uvIndex)"
+
+                        }
+                        
+                    } catch {
+                        
+                        print("JSON Processing Error.")
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tempLabel.text = ""
+        
+//        getCurrentTemperature(zipCode: 79922)
+//        
+//        let url = URL(string: "https://api.unsplash.com/photos/random?client_id=\(apiKeys.unsplashClientID)&page=1&query=snowy")
+//
+//        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+//
+//            if error != nil {
+//                print(error!)
+//            } else {
+//
+//                if let urlContent = data {
+//
+//                    do {
+//
+//                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+//
+////                        print(jsonResult)
+//
+////                        print(jsonResult["urls"])
+//
+//                        let urls = jsonResult["urls"] as? [String: String]
+//
+////                        print(urls)
+//                        if let urls = urls {
+//
+//                            if let fullURL = urls["full"] {
+//
+////                                print(fullURL)
+//
+//                                DispatchQueue.main.async {
+//
+//                                    let imageURL = URL(string: fullURL)
+//
+//                                    let imageData = try! Data(contentsOf: imageURL!)
+//
+//                                    self.image.image = UIImage(data: imageData)
+//
+//                                }
+//
+//
+//                            }
+//
+//                        }
+//
+//
+//                    } catch {
+//
+//                        print("JSON Processing Error.")
+//
+//                    }
+//
+//                }
+//
+//            }
+//
+//        }
+//        task.resume()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
